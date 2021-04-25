88250ef7cafb8921672dc9fe6c1a730e5f8aee75
tatatia <thetattianochka@gmail.com>
2021-04-25 20:23:23 +0300

    added components store and reducer

---
+++ src/components/Employees/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+//import { EMPLOYEES } from 'modules/api/endpoints';
+
+export default function Employees() {
+    return (
+        <div>
+            Employees Component
+        </div>
+    );
+}
---
+++ src/components/Jobs/index.jsx
@@ -0,0 +1,14 @@
+import api from 'modules/api/api';
+import { JOBS } from 'modules/api/endpoints';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+export default function Jobs() {
+    const state = useSelector(state => state)
+    console.log(state)
+    return (
+        <div>
+            Jobs Component
+        </div>
+    );
+}
